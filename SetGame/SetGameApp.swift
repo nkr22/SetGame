236fb228fddf8ccbe6d9a78a60bf91ad24c6486d
Noelia Root <noeliakroot@gmail.com>
2023-10-07 17:29:50 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Noelia Root on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
